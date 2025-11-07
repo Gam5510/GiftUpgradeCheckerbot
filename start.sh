0622a4a17ac92ad30e5d65b656453ee054019c96
Gam5510 <gameruz673@gmail.com>
2025-11-07 17:08:22 +0500

    Clean up start.sh by removing pip commands
    Removed pip upgrade and dependency installation steps.

--- start.sh
+++ start.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-set -e
-
-# Обновляем pip
-pip install --upgrade pip
-
-# Устанавливаем зависимости (только готовые бинарники)
-pip install --only-binary :all: -r requirements.txt
-
 # Запуск приложения
 exec uvicorn main:app --host ${HOST:-0.0.0.0} --port ${PORT:-8000}
